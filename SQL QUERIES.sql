745f9ccf4f3abcf9b028633fd731a273c499c0b8
subijith786 <subijithsubi53@gmail.com>
2024-11-09 11:32:06 +0530

    SQL QUERIES.sql

--- SQL QUERIES.sql
+++ SQL QUERIES.sql
@@ -31,16 +31,6 @@ where Global_Sales=82.53;
 select max(jp_sales) from video_game_sales;
 select * from video_game_sales
 where JP_Sales=6.5;
-select max(critic_score) from video_game_sales;
-select * from video_game_sales
-where Critic_Score=98;
-select max(user_score) from video_game_sales;
-select * from video_game_sales
-where User_Score=9.6;
-select `Name`,platform,year_of_release from video_game_sales
-where year_of_release=2015 and platform='PS3';
-select * from video_game_sales 
-where NA_Sales>10 and EU_Sales>10;
 
 #Top 5 games according to the global sales
 select Name,platform,Year_of_Release from video_game_sales
